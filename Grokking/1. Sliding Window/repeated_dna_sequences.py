c92626569ae3dca28473352498576639f7f62402
Akhil Kammila <akhilkammila@gmail.com>
2022-12-15 16:48:02 -0500

    setup venv, added gitignore and requirements.txt

---
+++ Grokking/1. Sliding Window/repeated_dna_sequences.py
@@ -0,0 +1,14 @@
+import collections
+
+
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        sequences = collections.defaultdict(int)
+        for i in range(len(s) - 9):
+            sequences[s[i:i+10]] += 1
+
+        output = []
+        for string, count in sequences.items():
+            if count > 1:
+                output.append(string)
+        return output
